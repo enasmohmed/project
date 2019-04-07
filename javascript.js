6a0934a08665fa6888c31c66d3dd8ec49a4a2887
Enas Mohamed <enasm2477@gmail.com>
2019-04-07 05:37:20 +0200

    project

---
+++ javascript.js
@@ -0,0 +1,9 @@
+function HoverButton()
+{
+      document.getElementById("butt").style.backgroundColor = "#080";
+}
+
+function Hover()
+{
+      document.getElementById("butt").style.backgroundColor = "#00BC8A";
+}
